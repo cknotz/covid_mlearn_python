6b5923395b098fe89d47a82f4cae0f94db3d3ee8
cknotz <carlo.knotz@gmail.com>
2020-08-13 17:18:48 +0200

    Reading in data

--- covid_keras_class.py
+++ covid_keras_class.py
@@ -6,12 +6,23 @@
 # Importing packages
 #!/usr/bin/env python3
 import csv
+import sys
 import os
 import numpy as np
 import pandas as pd
 
 
 # File path
-testtest
+print(os.getcwd())
+parpath = os.path.dirname(os.path.abspath('.'))
+path = os.path.join(parpath,
+                    'R Analysis',
+                    'covid_dashboard',
+                    'dashdata.dta')
+path
 
 # Reading dataset
+data = pd.read_stata(path)
+data.head()
+
+# Selecting relevant predictors
