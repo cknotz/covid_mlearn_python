430e050b83d0237becf5852433f7296f39e15fd3
cknotz <carlo.knotz@gmail.com>
2020-08-15 10:01:13 +0200

    Dummy coded all vars

--- covid_keras_class.py
+++ covid_keras_class.py
@@ -49,12 +49,17 @@ df.isna().sum().sum() # no
 # Data mangling
 ###############
 
-# convert four-digit postcode to one-digit
+# convert four-digit postcode to one-digit, then dummy-code
 df['ZIP'].describe()
 
 df['zip1'] = df["ZIP"].astype(str).str[:1].astype(int)
 df['zip1'].unique()
 del df['ZIP']
+
+zip = pd.get_dummies(df['zip1']).iloc[:,0:8]
+zip
+df = pd.concat([df,zip], axis = 1, sort = False)
+del df['zip1']
 
 # Check categorical vars (gender, diagnosis, region, edu, r_work before, income)
 df.dtypes
@@ -80,14 +85,27 @@ edu
 df = pd.concat([df, edu], axis = 1, sort = False)
 del df['resp_edu']
 
+# Income
+inc = pd.get_dummies(df['res_income']).iloc[:, 1:]
+inc
+df = pd.concat([df,inc], axis = 1, sort = False)
+del df['res_income']
 
-df.dtypes
+# Employment status
+work = pd.get_dummies(df['r_work_before']).iloc[:, 0:7] # leaving out 'other'
+work
+df = pd.concat([df,work], axis = 1, sort = False)
+del df['r_work_before']
 
-
-
-df['res_income'].unique()
+df.dtypes # looks ok
 
 # Preprocessing
+###############
 import sklearn as sk
+
+# Standardize
+
+# Convert to np array
+
 
 ##
