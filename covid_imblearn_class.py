4cba0e757cdc66466231a7fcf2c127911b225fa4
cknotz <carlo.knotz@gmail.com>
2020-09-19 18:13:43 +0200

    Finished preprocessing re-do (still OHE)

--- covid_imblearn_class.py
+++ covid_imblearn_class.py
@@ -14,9 +14,8 @@ import os
 import numpy as np
 import pandas as pd
 import sklearn as sk
-from sklearn.datasets import make_classification
+#from sklearn.datasets import make_classification
 import seaborn as sns
-import matplotlib as plt
 
 
 # Test case
@@ -84,3 +83,93 @@ df = df.join(zip)
 df.drop(['zip_num'],
         axis=1,
         inplace = True)
+
+# Income
+income = pd.get_dummies(df.res_income)
+income.head()
+
+df = df.join(income)
+df.drop(['res_income'],
+        axis = 1,
+        inplace = True)
+
+# Employment status
+work = pd.get_dummies(df.r_work_before)
+work.head()
+
+df = df.join(work)
+df.drop(['r_work_before'],
+        axis = 1,
+        inplace = True)
+
+# Education
+educ = pd.get_dummies(df.resp_edu)
+educ.head()
+
+df = df.join(educ)
+df.drop(['resp_edu'],
+        axis = 1,
+        inplace = True)
+
+
+# Gender
+df['resp_gender'].head()
+df.resp_gender = df.resp_gender.map({'Female':0,
+                              'Male':1})
+
+df['resp_gender'].head()
+df = df.rename(columns = {'resp_gender':'male'})
+
+# Region
+df['resp_region'].head()
+df.resp_region = df.resp_region.map({'french':1,
+                                     'german':0})
+
+df['resp_region'].head()
+df = df.rename(columns = {'resp_region':'french'})
+
+
+# Diagnosis
+df['resp_diag'].head()
+df.resp_diag = df.resp_diag.map({'Yes':1,
+                                 'No':0})
+
+list(df.columns)
+
+# Split off labels
+labels = df.resp_diag.copy()
+labels.head()
+
+features = df.drop(['resp_diag'],
+                   axis = 1)
+features.head()
+
+# Test-train split
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(features,
+                                                    labels,
+                                                    test_size = .2,
+                                                    random_state=17)
+
+# Check result
+print("Number of training samples:", len(X_train))
+print("Number of test samples:", len(X_test))
+
+
+# Analyze class imbalance
+counts = np.bincount(y_test)
+print(
+    "Number of positive samples in test data: {} ({:.2f}% of total)".format(
+        counts[1], 100 * float(counts[1]) / len(y_test)
+    )
+)
+counts = np.bincount(y_train)
+print(
+    "Number of positive samples in training data: {} ({:.2f}% of total)".format(
+        counts[1], 100 * float(counts[1]) / len(y_train)
+    )
+)
+# consider adjusting seed?
+
+# Logistic dry run
